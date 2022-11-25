a2c2bbdb55c7069a80a2f44ddbb4340a28163871
bikash-dev <bikash.dev.bk@gmail.com>
2022-11-25 23:02:01 +0600

    products category added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import AdvertisedItems from '../AdvertisedItems/AdvertisedItems';
 import Banner from '../Banner/Banner';
 import ProductsCategory from '../ProductsCategory/ProductsCategory';
+import Services from '../Services/Services';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner></Banner>
             <AdvertisedItems></AdvertisedItems>
             <ProductsCategory></ProductsCategory>
+            <Services></Services>
         </div>
     );
 };
--- src/Pages/Home/ProductsCategory/ProductsCategory.js
+++ src/Pages/Home/ProductsCategory/ProductsCategory.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ProductCard from './ProductCard';
 
 const ProductsCategory = () => {
+    const [products, setProducts] = useState([]);
+  
+    useEffect(() => {
+      fetch("http://localhost:5000/products")
+        .then((res) => res.json())
+        .then((data) => setProducts(data));
+    }, []);
     return (
-        <div>
-            <h2>This is product category</h2>
+        <div className='my-10'>
+            <h2 className='font-bold text-5xl text-center text-orange-600'>Choose your Brand</h2>
+            <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-6">
+                {
+                    products.map((product) => 
+                        <ProductCard key={product._id} product={product}></ProductCard>
+                    )
+                }
+            </div>
         </div>
     );
 };
