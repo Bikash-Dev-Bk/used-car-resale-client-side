5ac3f0482b64300300a4796570f77545bfeaa17c
bikash-dev <bikash.dev.bk@gmail.com>
2022-11-30 00:44:03 +0600

    delete added in my products

--- src/Pages/Dashboard/MyProducts/MyProducts.js
+++ src/Pages/Dashboard/MyProducts/MyProducts.js
@@ -1,56 +1,81 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { AuthContext } from '../../../contexts/AuthProvider';
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../../contexts/AuthProvider";
 
 const MyProducts = () => {
+  const { user } = useContext(AuthContext);
+  const [myProducts, setMyProducts] = useState([]);
 
-    const { user } = useContext(AuthContext);
+  useEffect(() => {
+    fetch(`http://localhost:5000/category/products/${user.email}`)
+      .then((res) => res.json())
+      .then((data) => setMyProducts(data));
+  }, [user.email]);
 
-    const [myProducts, setMyProducts] = useState([]);
-    // console.log("inside my products",myProducts);
-    useEffect(() => {
-        fetch(`http://localhost:5000/category/products/${user.email}`)
-        .then((res) => res.json())
-          .then((data) => setMyProducts(data));
-      },[user.email])
-
-    return (
-        <div>
-            <h3 className="text-3xl font-bold mb-5" >My Products</h3>
-            <div className="overflow-x-auto">
-            <table className="table w-full">
-              <thead>
-                <tr>
-                  <th>Product Name</th>
-                  <th>Price</th>
-                  <th>Sales Status</th>
-                  <th>Delete</th>
-                  <th>Advertise</th>
-                </tr>
-              </thead>
-    
-              {myProducts.map((product) => (
-                <tbody key={product._id}>
-                  
-                    <tr>
-                      <td>{product.name}</td>
-                      <td>{product.resale_price}</td>
-                      <td>
-                        Available
-                      </td>
-                      <td>
-                        <button className="btn btn-sm btn-error">Delete</button>
-                      </td>
-                      <td>
-                        <button className="btn btn-sm btn-outline btn-warning">Advertise</button>
-                      </td>
-                    </tr>
-                  
-                </tbody>
-              ))}
-            </table>
-          </div>
-        </div>
+  const handleDelete = (product) => {
+    const agree = window.confirm(
+      `Are you sure you want to delete: ${product.name}`
     );
+
+    if (agree) {
+      // console.log('deleting user with id: ', user._id)
+      fetch(`http://localhost:5000/category/products/${product._id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          // console.log(data)
+          if (data.deletedCount > 0) {
+            alert("Product deleted successfully.");
+            const remainingProducts = myProducts.filter(
+              (prod) => prod._id !== product._id
+            );
+            setMyProducts(remainingProducts);
+          }
+        });
+    }
+  };
+
+  return (
+    <div>
+      <h3 className="text-3xl font-bold mb-5">My Products</h3>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th>Product Name</th>
+              <th>Price</th>
+              <th>Sales Status</th>
+              <th>Delete</th>
+              <th>Advertise</th>
+            </tr>
+          </thead>
+
+          {myProducts.map((product) => (
+            <tbody key={product._id} product={product}>
+              <tr>
+                <td>{product.name}</td>
+                <td>{product.resale_price}</td>
+                <td>Available</td>
+                <td>
+                  <button
+                    onClick={() => handleDelete(product)}
+                    className="btn btn-sm btn-error"
+                  >
+                    Delete
+                  </button>
+                </td>
+                <td>
+                  <button className="btn btn-sm btn-outline btn-warning">
+                    Advertise
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          ))}
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default MyProducts;
