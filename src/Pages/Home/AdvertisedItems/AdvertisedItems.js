e85d13ca48a33740071d175ab1b1e1c1782670ba
bikash-dev <bikash.dev.bk@gmail.com>
2022-11-30 23:32:08 +0600

    advertise item added

--- src/Pages/Home/AdvertisedItems/AdvertisedItems.js
+++ src/Pages/Home/AdvertisedItems/AdvertisedItems.js
@@ -2,21 +2,44 @@ import React, { useEffect, useState } from "react";
 
 const AdvertisedItems = () => {
   const [advertisedItems, setAdvertisedItems] = useState([]);
+  console.log("INSIDE ADVERTISE",AdvertisedItems);
 
   useEffect(() => {
     fetch(`http://localhost:5000/category/products/advertise`)
       .then((res) => res.json())
-      .then((data) => setAdvertisedItems(data));
+      .then((data) => {
+        setAdvertisedItems(data)
+        console.log("INSIDE ADVERTISE",data);
+      });
   }, []);
   return (
     <>
-      {advertisedItems.length && (
-        <div className="h-96">
-          <h2 className="text-6xl font-bold text-center my-10 text-orange-600">
-            This is Advertised Items
-          </h2>
-        </div>
+    {
+      advertisedItems.length &&
+      <div >
+    <h2 className="text-6xl font-bold text-center my-10 text-orange-600">
+      Advertised Items
+    </h2>
+    
+      { (
+        
+          advertisedItems.map(advertisedItem =>{
+            return (
+            
+          <div className="card card-side bg-base-100 shadow-xl mb-5">
+            <img width="300px" src={advertisedItem.img} alt="Movie"/>
+            <div className="card-body">
+              <h2 className="card-title font-bold text-5xl">{advertisedItem.name}</h2>
+              <p className="font-bold text-4xl"><span className="text-orange-600">Price:</span> {advertisedItem.resale_price}</p>
+            </div>
+          </div>
+        )
+          })
+        
+        
       )}
+      </div>
+}
     </>
   );
 };
