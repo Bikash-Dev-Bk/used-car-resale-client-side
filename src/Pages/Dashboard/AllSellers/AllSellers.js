4d10daa1f7fad51f12bffd6c7331b189ba14a14b
bikash-dev <bikash.dev.bk@gmail.com>
2022-11-30 13:58:00 +0600

    all sellers delete button

--- src/Pages/Dashboard/AllSellers/AllSellers.js
+++ src/Pages/Dashboard/AllSellers/AllSellers.js
@@ -1,52 +1,73 @@
-import React from 'react';
-import { useQuery } from '@tanstack/react-query';
+import React, { useEffect, useState } from "react";
 
 const AllSellers = () => {
-    const {data: users = [], } = useQuery({
-        queryKey: ['users'],
-        queryFn: async() =>{
-            const res = await fetch('http://localhost:5000/users');
-            const data = await res.json();
-            return data;
-        }
-    });
-    console.log("inside allSellers",users);
-    return (
-        <div>
-          <h3 className="text-3xl mb-5">All Seller</h3>
-          <div className="overflow-x-auto">
-            <table className="table w-full">
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Email</th>
-                  <th>Delete</th>
-                  <th>Verify</th>
-                </tr>
-              </thead>
-    
-              {users.map((user) => (
-                <tbody key={user._id}>
-                  {
-                    user.userType === 'seller' &&
-                    <tr>
-                      <td>{user.name}</td>
-                      <td>{user.email}</td>
-                      <td>
-                        <button className="btn btn-xs btn-error">Delete</button>
-                      </td>
-                      <td>
-                        <button className="btn btn-xs btn-success">Verify</button>
-                      </td>
-                    </tr>
-                  }
-                </tbody>
-              ))}
-            </table>
-          </div>
-        </div>
-    );
-};
+  const [users, setUsers] = useState([]);
 
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  const handleDelete = (user) => {
+    const agree = window.confirm(
+      `Are you sure you want to delete: ${user.name}`
+    );
+
+    if (agree) {
+      fetch(`http://localhost:5000/users/${user._id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.deletedCount > 0) {
+            alert(`${user.userType} deleted successfully.`);
+            const remainingUsers = users.filter((usr) => usr._id !== user._id);
+            setUsers(remainingUsers);
+          }
+        });
+    }
+  };
+
+  return (
+    <div>
+      <h3 className="text-3xl mb-5">All Seller</h3>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Delete</th>
+              <th>Verify</th>
+            </tr>
+          </thead>
+
+          {users.map((user) => (
+            <tbody key={user._id}>
+              {user.userType === "seller" && (
+                <tr>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>
+                    <button
+                      onClick={() => handleDelete(user)}
+                      className="btn btn-xs btn-error"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                  <td>
+                    <button className="btn btn-xs btn-success">Verify</button>
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          ))}
+        </table>
+      </div>
+    </div>
+  );
+};
 
 export default AllSellers;
